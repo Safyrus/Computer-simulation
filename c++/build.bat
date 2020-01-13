2902e6456dddcac757982c98f097df13ef42bff5
Safyrus <54881649+Safyrus@users.noreply.github.com>
2020-01-13 07:24:50 +0100

    update build.bat

--- c++/build.bat
+++ c++/build.bat
@@ -5,6 +5,6 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxi
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 )
 set compilerflags=/Od /Zi /EHsc /std:c++latest /I include
-set linkerflags=/OUT:bin\main.exe
-cl.exe %compilerflags% src\*.cpp /link %linkerflags%
-del bin\*.ilk *.obj *.pdb
+set linkerflags=/OUT:c++\bin\main.exe
+cl.exe %compilerflags% c++\src\*.cpp /link %linkerflags%
+del c++\bin\*.ilk *.obj *.pdb
