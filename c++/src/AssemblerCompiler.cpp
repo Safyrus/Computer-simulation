849e95260d0e98c7c88b6e2d15a234b06577971d
Safyrus <54881649+Safyrus@users.noreply.github.com>
2020-01-14 19:32:08 +0100

    fix labels and comments detection

--- c++/src/AssemblerCompiler.cpp
+++ c++/src/AssemblerCompiler.cpp
@@ -27,16 +27,17 @@ AssemblerCompiler::~AssemblerCompiler()
 {
 }
 
-void AssemblerCompiler::loadAssembler(const char* f)
+void AssemblerCompiler::loadAssembler(const char *f)
 {
     //open file
-    std::ifstream file (f);
+    std::ifstream file(f);
 
     if (!file.is_open())
     {
         std::cout << "Error: Unable to open file\n";
         return;
-    }else
+    }
+    else
     {
         std::cout << "file open \n";
     }
@@ -46,60 +47,77 @@ void AssemblerCompiler::loadAssembler(const char* f)
 
     //-----label reading-----
     // for each line
-    while (getline(file,line))
+    while (getline(file, line))
     {
         std::string word;
+        bool comment = false;
+        bool label = false;
 
         //for each word
         while (line != "")
         {
             //check if end of line
             word = line.substr(0, line.find(" "));
-            if(word==line)
+            if (word == line)
             {
                 line = "";
-            }else
+            }
+            else
             {
-                line = line.substr(line.find(" ")+1);
+                line = line.substr(line.find(" ") + 1);
             }
             std::cout << word << " ";
 
+            if (!comment && word.find(char_comment) != std::string::npos)
+            {
+                comment = true;
+            }
+
+            if(label && !comment)
+            {
+                std::cout << "**ERROR: SOMETHING AFTER LABEL**" << line;
+                line = "";
+            }
 
             //label symbol find a the end of the word
-            if(word.back()== char_label && word.front()!=char_comment)
+            if (word.back() == char_label && !comment)
             {
                 std::cout << "**LABEL DETECTED**";
-                word = word.substr(0, word.length()-1);
-                
+                word = word.substr(0, word.length() - 1);
+
                 //special char like reg or dec find
-                if(word.find(char_comment)!=std::string::npos || word.find(char_reg)!=std::string::npos || word.find(char_decimal)!=std::string::npos || word.find(char_labelReplace)!=std::string::npos ||  word.find(char_label)!=std::string::npos)
+                if (word.find(char_comment) != std::string::npos || word.find(char_reg) != std::string::npos || word.find(char_decimal) != std::string::npos || word.find(char_labelReplace) != std::string::npos || word.find(char_label) != std::string::npos)
                 {
                     std::cout << "**ERROR: LABEL NAME INCORRECT**";
-                }else
+                }
+                else
                 {
                     //add label
-                        std::cout << "**ADD LABEL**";
+                    std::cout << "**ADD LABEL**";
                     labels.push_back(word);
                     labels_pos.push_back(lineCount);
                     std::cout << labels.back();
+                    label = true;
                 }
             }
         }
         std::cout << '\n';
         lineCount++;
     }
-    
+
     //close file
     file.close();
     std::cout << "file close\n";
 
-    std::cout << "#------------------------------#\n";
+    //print result
+    std::cout << "#------------------------------#\n|Labels:\n";
     for (int i = 0; i < labels.size(); i++)
     {
-        std::cout << labels.at(i) << ": " << labels_pos.at(i) <<'\n';
+        std::cout << labels_pos.at(i) << ": " << labels.at(i) << '\n';
     }
     std::cout << "#------------------------------#\n";
-    
+
+    std::cin.ignore();
 
     //open file
     file.open(f);
@@ -107,106 +125,105 @@ void AssemblerCompiler::loadAssembler(const char* f)
     {
         std::cout << "Error: Unable to open file\n";
         return;
-    }else
+    }
+    else
     {
         std::cout << "file open 2\n";
     }
-    
+
     lineCount = 0;
 
     //instruction and comment reading
     // for each line
-    while (getline(file,line))
+    while (getline(file, line))
     {
         std::string word;
-
 
         int8_t cmd_arg = 0;
         int8_t cmd_argLabel = 0;
         bool comment = false;
-        bool label = false;
         int8_t cmd_index = 0;
+        bool label = false;
+        //set label to true if this line contain label
+        for (int i = 0; i < labels_pos.size(); i++)
+        {
+            if(labels_pos[i]==lineCount)
+            {
+                label = true;
+                break;
+            }
+        }
+        
 
         //for each word
         while (line != "")
         {
             //check if end of line
             word = line.substr(0, line.find(" "));
-            if(word==line)
+            if (word == line)
             {
                 line = "";
-            }else
+            }
+            else
             {
-                line = line.substr(line.find(" ")+1);
+                line = line.substr(line.find(" ") + 1);
             }
             std::cout << word << " ";
 
-            /*if(comment)
-            {
-                // add comment
-                comments.push_back(word);
-                comments_pos.push_back(lineCount);
-            }*/
-
             //comment symbol find
-            if(word.find(char_comment)!=std::string::npos)
+            if (word.find(char_comment) != std::string::npos)
             {
                 std::cout << "**COMMENT DETECTED**";
                 int char_com_pos = word.find(char_comment);
-                std::string strBefor = word.substr(0, (char_com_pos<0)?0:char_com_pos);
-                std::string strAfter = word.substr(word.find(char_comment)+1);
-                if(strBefor != "")
+                std::string strBefor = word.substr(0, (char_com_pos < 0) ? 0 : char_com_pos);
+                std::string strAfter = word.substr(word.find(char_comment) + 1);
+                if (strBefor != "")
                 {
                     std::cout << "**SOMETHING BEFOR COMMENT**\n";
                     line = strBefor + ' ' + char_comment + strAfter + " " + line;
-                }else
+                }
+                else
                 {
-                    //comment = true;
-                    if(strAfter != "")
-                    {
-                        std::cout << "**ADD COMMENT**" << strAfter + line;
-                        comments.push_back(strAfter + line);
-                        comments_pos.push_back(lineCount);
-                        line = "";
-                    }
+                    comment = true;
+                    std::cout << "**ADD COMMENT**" << strAfter + " " + line;
+                    comments.push_back(strAfter + " " + line);
+                    comments_pos.push_back(lineCount);
+                    line = "";
                 }
             }
+            //check if label is on the line
             else if (label)
             {
-                // error: something wrong after label
+                std::cout << "**LABEL FIND**";
             }
-            else if (false/* label symobl find */)
+            //check if any arg remaind
+            else if (cmd_arg < op_arg[cmd_index])
             {
-                label = true;
-                // ignore
-            }
-            else if (cmd_arg<op_arg[cmd_index])
-            {
-                if(cmd_argLabel<op_label[cmd_index] && op_label_pos[cmd_index][cmd_argLabel]==cmd_arg)
+                if (cmd_argLabel < op_label[cmd_index] && op_label_pos[cmd_index][cmd_argLabel] == cmd_arg)
                 {
                     bool find = false;
-                    while (false/* check label */)
+                    while (false /* check label */)
                     {
-                        if(false/* label exist */)
+                        if (false /* label exist */)
                         {
                             //replace label
                             find = true;
                             break;
                         }
                     }
-                    if(!find)
+                    if (!find)
                     {
                         //error: label does not exist
                     }
-                    //add label                    
+                    //add label
                     cmd_argLabel++;
                     cmd_arg++;
                 }
                 else
                 {
-                    if (false/* register char find */)
+                    if (false /* register char find */)
                     {
-                        if(false/* convert error */)
+                        if (false /* convert error */)
                         {
                             //error: not a valid register name
                         }
@@ -215,9 +232,9 @@ void AssemblerCompiler::loadAssembler(const char* f)
                             //add reg to instruction
                         }
                     }
-                    else if (false/* decimal char find */)
+                    else if (false /* decimal char find */)
                     {
-                        if(false/* convert error */)
+                        if (false /* convert error */)
                         {
                             //error: not a valid decimal number
                         }
@@ -225,9 +242,10 @@ void AssemblerCompiler::loadAssembler(const char* f)
                         {
                             //add dec to instruction
                         }
-                    }else
+                    }
+                    else
                     {
-                        if(false/* convert error */)
+                        if (false /* convert error */)
                         {
                             //error: not a valid hexadecimal number
                         }
@@ -238,12 +256,13 @@ void AssemblerCompiler::loadAssembler(const char* f)
                     }
                 }
             }
-            else if (false/* command find */)
+            //command find (maybe)
+            else
             {
-                int i=0;
-                while (false/* command name not find */)
+                int i = 0;
+                while (false /* command name not find */)
                 {
-                    if(false/* cmd name find */)
+                    if (false /* cmd name find */)
                     {
                         // add command
                         cmd_index = i;
@@ -252,14 +271,11 @@ void AssemblerCompiler::loadAssembler(const char* f)
                 }
                 //error: not a command name
             }
-            else
-            {
-                //error: something is wrong, maybe a space or something
-            }            
         }
         std::cout << '\n';
+        lineCount++;
 
-        if(!comment && !label)
+        if (!comment && !label)
         {
             for (int i = op_arg[cmd_index]; i < op_size; i++)
             {
@@ -271,6 +287,19 @@ void AssemblerCompiler::loadAssembler(const char* f)
     //close file
     file.close();
     std::cout << "file close 2\n";
+
+    //print result
+    std::cout << "#------------------------------#\n|Labels:\n";
+    for (int i = 0; i < labels.size(); i++)
+    {
+        std::cout << labels_pos.at(i) << ": " << labels.at(i) << '\n';
+    }
+    std::cout << "|Comments:\n";
+    for (int i = 0; i < comments.size(); i++)
+    {
+        std::cout << comments_pos.at(i) << ": " << comments.at(i) << '\n';
+    }
+    std::cout << "#------------------------------#\n";
 }
 
 void AssemblerCompiler::loadBinary(const char *f)
--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -2,13 +2,13 @@
 #include "AssemblerCompiler.hpp"
 
 int main() {
-    std::cout << "Test for assemblerCompiler" << std::endl;
+    std::cout << "-=#[ Test for assemblerCompiler ]#=-" << std::endl;
     std::cin.ignore();
 
     AssemblerCompiler *compiler = new AssemblerCompiler();
     compiler->loadAssembler("testCompiler.sac");
     delete compiler;
 
-    std::cout << "Done" << std::endl;
+    std::cout << "-=#[ Done ]#=-" << std::endl;
     std::cin.ignore();
 }
