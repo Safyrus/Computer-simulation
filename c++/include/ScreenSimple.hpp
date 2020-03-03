77afeefba6088b3e7712b23e988f81b435443a4e
VisentiniEmilien <54881649+Safyrus@users.noreply.github.com>
2020-03-03 20:44:01 +0100

    added a new simple screen

---
+++ c++/include/ScreenSimple.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+
+#include <SFML/Graphics.hpp>
+
+#include "Device.hpp"
+
+class ScreenSimple : public Device
+{
+private:
+    const int W = 128;
+    const int H = 128;
+    sf::RenderTexture RT;
+    std::vector<sf::Vertex> pixMat;
+
+    sf::Color color[16];
+public:
+    ScreenSimple();
+    ~ScreenSimple();
+
+    void setData(int8_t d);
+
+    void display(sf::RenderWindow &window, int x, int y);
+};
